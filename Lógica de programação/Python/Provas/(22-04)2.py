80d30a86638e040e4f821917dca8161073d2d6e6
Daviramos7 <daviramosferr2015@gmail.com>
2024-05-14 16:25:51 -0300

    correção do código e adcionada a função de calcular a média de altura

--- Lógica de programação/Python/Provas/(22-04)2.py
+++ Lógica de programação/Python/Provas/(22-04)2.py
@@ -1,51 +1,50 @@
 def ler_dados_pessoa():
-    nome = input
-    ("Digite o seu nome: ")
-    salario = float(input("Digite o  salário: "))
+    nome = input("Digite o seu nome: ")
+    salario = float(input("Digite o seu salário: "))
     peso = float(input("Digite o peso: "))
-    altura = float (input("Digite a altura: "))
+    altura = float(input("Digite a altura da pessoa: "))
     return (nome, salario, peso, altura)
 
-def pessoa_mais_alta (pessoas):
+def pessoa_mais_alta(pessoas):
     maior_altura = 0.0
     pessoa_mais_alta = None
 
     for pessoa in pessoas:
-        nome, _, _,
-        altura = pessoa
+        nome, _, _, altura = pessoa
         if altura > maior_altura:
             maior_altura = altura
             pessoa_mais_alta = nome
 
-    return
-    pessoa_mais_alta, maior_altura
+    return pessoa_mais_alta, maior_altura
 
-def calcular_medias (pessoas):
+def calcular_medias(pessoas):
     total_salarios = sum(pessoa[1] for pessoa in pessoas)
     total_pesos = sum(pessoa[2] for pessoa in pessoas)
-    qtd_pessoas = len (pessoas)
+    total_altura = sum(pessoa[3] for pessoa in pessoas)
+    qtd_pessoas = len(pessoas)
 
     media_salario = total_salarios / qtd_pessoas
     media_peso = total_pesos / qtd_pessoas
-
-    return
-    (media_salario, media_peso)
+    media_altura = total_altura / qtd_pessoas
+    
+    return(media_salario, media_peso, media_altura)
 
 def main():
-    pessoas []
+    pessoas = []
 
     for _ in range(3):
         pessoa = ler_dados_pessoa()
         pessoas.append(pessoa)
+    
+    mais_alta, altura_mais_alta = pessoa_mais_alta(pessoas)
 
-    mais_alta, altura_mais_alta = pessoa_mais_alta (pessoas)
+    media_salario, media_peso, media_altura = calcular_medias(pessoas)
 
-    media_salario, media_peso = calcular_medias (pessoas)
-
-    print(f"A pessoa mais alta é: {mais_alta}, com a altura de {altura_mais_alta} metros.")
+    print(f"A pessoa mais alta é: {mais_alta} com altura de {altura_mais_alta} metros.")
     print(f"A média de salário das três pessoas é: {media_salario:.2f}")
-    print(f"A média de peso das três pessoas é {media_peso:.2f}")
+    print(f"A média de peso das três pessoas é: {media_peso:.2f}")
+    print(f"A média de altura das três pessoas é: {media_altura:.2f}")
 
-    if __name__ ==
-    "__main__" :
-        main()
+if __name__ == "__main__" :
+    main()
+    
