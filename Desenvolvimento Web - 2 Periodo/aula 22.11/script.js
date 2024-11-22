394f66e6a09c298ed2703f42fc221501d38fe806
Davi Ramos Ferreira <121807492+Daviramos7@users.noreply.github.com>
2024-11-22 12:53:26 +0000

    aula dia 22/11

---
+++ Desenvolvimento Web - 2 Periodo/aula 22.11/script.js
@@ -0,0 +1,7 @@
+function abre(){
+    document.getElementsByClassName('menu-container')[0].style.width = '75%'
+}
+
+function fechar(){
+    document.getElementsByClassName('menu-container')[0].style.width = '0%'
+}
